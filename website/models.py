d5542f6f8796ce1a7226e7e6449b8e59fd9041bb
pbansal426 <151982162+pbansal426@users.noreply.github.com>
2024-08-31 06:24:28 +0000

    databases for User and School

---
+++ website/models.py
@@ -0,0 +1,22 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10000))
+    zip = db.Column(db.DateTime(timezone=True), default=func.now())
+    contact_name = db.Column(db.String(1000))
+    contact_email=db.Column(db.String(1000))
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(1500))
+    question = db.Column(db.String(150000))
+    answer = db.Column(db.String(100000))
+    #notes = db.relationship('')
+    
+    
+
+    
